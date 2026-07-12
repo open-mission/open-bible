7256edd1a74a73de6e07e4c663e39865857755d5
Claudio Alberto <cafg.dev@gmail.com>
2026-07-12 18:02:09 -0300

    improve(reader): add settings button in simple mode header (#143) (#144)

--- features/workspace/components/simple-home.tsx
+++ features/workspace/components/simple-home.tsx
@@ -109,6 +109,7 @@ export function SimpleHome() {
                     onChangeVerseSpacing={setVerseSpacing}
                     readerFont={readerFont}
                     onChangeReaderFont={setReaderFont}
+                    showConfigButton={true}
                   />
                 ) : (
                   <ReaderEmpty
